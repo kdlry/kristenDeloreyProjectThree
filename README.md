9abc44710d5a48df38c0c93118b74014fa52bd02
kdlry <kristen.delorey@live.com>
2020-09-14 23:37:21 -0400

    final changes for submissions

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# kristenDeloreyProjectThree
+Project link: https://kdlry.github.io/kristenDeloreyProjectThree/
+
+Successes:
+Being new to JS/jQuery, there was a lot of trial and error to get some of the simplest functions to work.  I learned SO much along the way and made a few breakthroughs - including html2canvas.
+
+Room for Improvement:
+Cleaner (dry-er) code and getting better at responsive design - smoother transitions and fewer breakpoints.
--- script.js
+++ script.js
@@ -1,64 +1,66 @@
 // Name space object----------------------------
 const botUserInput = {}; 
 
-
 // Functions definiitons ----------------------------
-
-// Create function to display images based on user input
+// Create function to display images triggered by user input
 botUserInput.showInput = function() {
 
     // Bind event listener that looks for a change on all form inputs
     $('input').on('change', function() {
     
-        // Create variable that stores selected input with an id
-        let val = this.id;
+        // Create variable that stores selected form input with an id
+        // let val = this.id;
+        let val = $(this).attr('id');
 
-        // Create variable that stores a class value for selected input with an id
+        // Create a second variable that gets linked image class for selected form input id
         let selectedInput = $(`.${val}`);
 
-        // Look for sibling inputs and hide the associated images from display (radio buttons)
+        // For selected images with direct siblings, remove siblings from display and only show target image (radio buttons - botColour + botScene)
         selectedInput.siblings().removeClass('show');
 
-        // Toggle class on inputs to view and hide images on demand (checkboxes)
+        // For selected images without siblings, toggle view/hide target images on demand (checkboxes - botSwag)
         selectedInput.toggleClass('show');
     }); 
 }
 
-// Create function to reset form input and clear images from display
+// Create function to reset form inputs and clear images from display
 botUserInput.resetInputs = function() {
 
     // Bind event listener that looks for a click on reset button
     $('button').on('click', function() {
         
-        // Remove checked from inputs that were checked
+        // Remove all form inputs that were checked
         $('input').prop('checked', false);
         
-        // Remove the class of show from all images in the .botImages div
+        // Remove all images with a class of show
         $('.iceBot').removeClass('show');
         $('.peachBot').removeClass('show');
         $('.violetBot').removeClass('show');
-        $(".rosetBot").removeClass("show");
-        $(".dayScene").removeClass("show");
-        $(".nightScene").removeClass("show");
-        $(".swagTopHat").removeClass("show");
-        $(".swagShades").removeClass("show");
-        $(".swagIceCream").removeClass("show");
-        $(".swagChain").removeClass("show");
-        $(".swagShoes").removeClass("show");
+        $('.rosetBot').removeClass('show');
+        $('.dayScene').removeClass('show');
+        $('.nightScene').removeClass('show');
+        $('.swagTopHat').removeClass('show');
+        $('.swagShades').removeClass('show');
+        $('.swagIceCream').removeClass('show');
+        $('.swagChain').removeClass('show');
+        $('.swagShoes').removeClass('show');
 
-        $(".chk").prop("disabled", true);
+        // Restore disabled property on checkboxes
+        $('.chk').prop('disabled', true);
 
-        $("body").find("canvas").remove();
+
+        //Remove canvas from display section 
+        $('body').find('canvas').remove();
     })
 }
 
-// Create function that enables swag inputs once a swatch has been selected
+// Create function that enables swag inputs once any swatch input has been selected
 botUserInput.enableSwag = function() {   
     
     // Bind event listener that looks for a change on swatch inputs
     $('.swatch input').on('change', function(){
     
-        // When change is made on a swatch input, enable swag inputs
+        // When a change is made, enable swag inputs
         $('.swag input').prop('disabled', false);
         
     });
@@ -67,10 +69,10 @@ botUserInput.enableSwag = function() {
 // Create function that activates modal when swag inputs are selected before a swatch has been selected
 botUserInput.activateAlert = function () {
     
-    // Bind event listener that looks for a click on swag inputs
+    // Bind event listener that looks for a click on disabled swag inputs
     $('.swag label').on('click', function () {
 
-        // When a swag input is clicked on, activate (call) alert
+        // When a swag input is clicked on, activate the modal
         if ($('.swag input').prop('disabled') === true) {
             
             // Alert content
@@ -85,23 +87,23 @@ botUserInput.activateAlert = function () {
 // Credit for code below: http://html2canvas.hertzen.com/
 // http://adnan-tech.com/save-div-as-image-html2canvas/
 
-// Create function that captures div of images selected on inputs
+// Create function that captures div of images selected from form inputs
 botUserInput.captureImg = function() {
 
     // Bind event listener that looks for a click on the camera icon
     $('.screenCap').on('click', function(e) {
         e.preventDefault();
-        // botUserInput.modal();
 
-        // Move the scroll on top of page (this is a necessary step to carry out html2canvas script)
+        // Move scroll to the top of the page (this is a required step to carry out html2canvas script below)
         window.scrollTo(0, 0);
 
-        // Convert the div to an image (canvas) and append to the bottom of the body
-        html2canvas(document.getElementById("capture")).then(function (canvas) {
+        // Convert selected images in div to a canvas (captured image)
+        html2canvas(document.getElementById('capture')).then(function (canvas) {
         
-            // document.querySelector("footer").appendChild(canvas);
-            document.getElementById("capturedImg").appendChild(canvas);
+            // Append canvas to the display section
+            document.getElementById('capturedImg').appendChild(canvas);
 
+            // Move scroll to displayed canvas
             let element = (canvas);
             element.scrollIntoView();
         });
@@ -116,14 +118,13 @@ botUserInput.init = function() {
     botUserInput.activateAlert();
     botUserInput.captureImg();
 
-    // When the viewer's window is less than of equal to 630 in width, alert them to move to landscape for tablet/mobile
+    // When the viewer's window is less than of equal to 630 in width, alert them to move to landscape view
     if($(window).width() <= 650){
         
-        // Display a sweet alert
+        // Display sweet alert with custom content
         swal({
-            title: 'Turn to landscape view for a less than stellar (but better than portrait) experience!',
+            title: 'Turn to landscape view for a less than stellar (but better than portrait!) experience',
             button: 'Okay.',
-            onOpen: () => Swal.getConfirmButton().focus()
         });
     }
 }
